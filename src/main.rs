5bd2de77b8ba6268fd5dbbd3c1c0a3192c6b555b
NomisIV <simon@nomisiv.com>
2021-06-30 11:25:00 +0200

    Change main return type to simplify error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::env::args;
 use std::process::Command;
-use swayipc::Connection;
+use swayipc::{Connection, Error};
 
-fn main() {
+fn main() -> Result<(), Error> {
     // Prepare command
     let args: Vec<String> = args().collect();
     let child_process_name = args.get(1).expect("No process to start");
@@ -12,12 +12,11 @@ fn main() {
     }
 
     // Set up connection
-    let mut con = Connection::new().expect("Could not connect to sway socket");
+    let mut con = Connection::new()?;
 
     // Get pid from focused window
     let pid: i32 = con
-        .get_tree()
-        .expect("Could not retreive window tree")
+        .get_tree()?
         .find_focused(|node| node.focused == true)
         .expect("No focused node")
         .pid
@@ -26,8 +25,7 @@ fn main() {
     let mark = format!("hidden-{}", pid);
 
     // Mark window and move to scratchpad
-    con.run_command(format!("mark {}; move scratchpad", mark))
-        .expect("Could not move window to scratchpad");
+    con.run_command(format!("mark {}; move scratchpad", mark))?;
 
     // Run command
     let output = child_process.status();
@@ -36,8 +34,7 @@ fn main() {
     con.run_command(format!(
         "[pid={}] focus; move mark {}; floating disable",
         pid, mark
-    ))
-    .expect("Could not send window back to workspace");
+    ))?;
 
     match output {
         Ok(output) => match output.code() {
@@ -46,4 +43,6 @@ fn main() {
         },
         Err(_) => println!("Failed to start {}", child_process_name),
     }
+
+    Ok(())
 }
